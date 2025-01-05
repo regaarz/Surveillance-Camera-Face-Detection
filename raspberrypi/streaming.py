0cc0d6247623e2c97874303951d070b17bb98183
Rega Arzula Akbar <108121767+regaarz@users.noreply.github.com>
2025-01-05 22:01:53 +0700

    Add files via upload

---
+++ raspberrypi/streaming.py
@@ -0,0 +1,107 @@
+
+
+import cv2
+import io
+import logging
+import socketserver
+import numpy as np
+from http import server
+from threading import Condition, Thread
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
+
+PAGE = """\
+<html>
+<head>
+<title>picamera2 MJPEG streaming demo</title>
+</head>
+<body>
+<h1>Picamera2 MJPEG Streaming Demo</h1>
+<img src="stream.mjpg" width="640" height="480" />
+</body>
+</html>
+"""
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888',"size": (3264, 2448)}))
+picam2.start()
+
+
+output = StreamingOutput()
+
+map_x = np.load('grid_x_entaniya.npz')['arr_0'].astype(np.float32)
+map_y = np.load('grid_y_entaniya.npz')['arr_0'].astype(np.float32)
+
+def capture_frames():
+    while True:
+        im = picam2.capture_array()
+        frame = cv2.remap(cv2.resize(im, (2592, 1944)), map_x, map_y, interpolation=cv2.INTER_LINEAR)
+        _,jpeg_frame = cv2.imencode('.jpg',frame)
+        output.write(jpeg_frame.tobytes())
+        
+capture_thread = Thread(target=capture_frames,daemon=True)
+capture_thread.start()
+
+try:
+    address = ('', 8000)
+    server = StreamingServer(address, StreamingHandler)
+    server.serve_forever()
+finally:
+    picam2.stop_recording()
+
+
